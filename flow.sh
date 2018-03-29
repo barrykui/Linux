b5a34fc289d3908eaf64db436d4593cf54f82972
Jack <sjkai1@126.com>
2018-03-29 17:16:31 +0800

    Add files via upload

--- flow.sh
+++ flow.sh
@@ -1,19 +1,24 @@
 #!bin/sh
 
 # script for using https://github.com/Jiankai-Sun/dense_flow
-DIR=' '
-DEST=' '
+VIDEO_DIR=/Disk1/20bn-datasets/tmp/20bn-something-something/
+RGB_DEST=/Disk1/20bn-something-something/20bn-something-something-v1_rgb/
+FLOW_DEST=/Disk1/20bn-something-something/20bn-something-something-v1_flow/
 DENSE_FLOW=/home/jack/dense_flow/build/extract_gpu
 
-if [ ! -d "$DEST" ]; then
-  mkdir $DEST
+if [ ! -d "${RGB_DEST}" ]; then
+  mkdir -p ${RGB_DEST}
+fi
+if [ ! -d "${FLOW_DEST}" ]; then
+  mkdir -p ${FLOW_DEST}
 fi
 
-for video in $DIR
+for video in ${VIDEO_DIR}
 do
-    if test -f $video
+    if test -f ${video}
     then
-        mkdir $DEST$video
-        $DENSE_FLOW -f $DIR$video -x $DEST$video/flow_x -y $DEST$video/flow_y -i $DEST$video/image -b 20 -t 1 -d 0 -s 1 -o dir
+        mkdir ${RGB_DEST}${video}
+        mkdir ${FLOW_DEST}${video}
+        ${DENSE_FLOW} -f ${VIDEO_DIR}${video} -x ${FLOW_DEST}${video}/${video}-x -y ${FLOW_DEST}${video}/${video}-y -i ${RGB_DEST}${video}/${video}- -b 20 -t 1 -d 0 -s 1 -o dir
     fi
 done
