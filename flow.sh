4044cade35d59dedc036da28b92dded96852b911
Jack <sjkai1@126.com>
2018-03-29 16:46:35 +0800

    Add files via upload

--- flow.sh
+++ flow.sh
@@ -14,11 +14,6 @@ do
     if test -f $video
     then
         mkdir $DEST$video
-        echo $file 是文件
         $DENSE_FLOW -f $DIR$video -x $DEST$video/flow_x -y $DEST$video/flow_y -i $DEST$video/image -b 20 -t 1 -d 0 -s 1 -o dir
-    fi
-    if test -d $file
-    then
-        echo $file 是目录
     fi
 done
